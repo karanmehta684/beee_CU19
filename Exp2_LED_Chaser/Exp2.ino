4079e6ffe21219965ab7044bdf97f2b4b9ba1f52
karanmehta684 <55104368+karanmehta684@users.noreply.github.com>
2019-11-08 00:17:14 +0530

    Create Exp2.ino

---
+++ Exp2_LED_Chaser/Exp2.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+void loop()
+{ 
+  for(int i=1;i<=4;i++)
+  {
+     digitalWrite(i,HIGH);
+  	 delay(500);
+  	 digitalWrite(i,LOW);
+     delay(500);
+  }
+}
